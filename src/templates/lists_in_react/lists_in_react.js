2d2ecd338d2c0a2b0b2e1fdb46f644b0a3ead60b
jezna <jeznacki13@gmail.com>
2021-10-29 14:14:00 +0200

    Lists in react

--- src/templates/lists_in_react/lists_in_react.js
+++ src/templates/lists_in_react/lists_in_react.js
@@ -5,17 +5,22 @@ function View_ListsInReact() {
     return <div className='cont'>
         <header>
             <h2>Lists in React</h2>
-
+            <p>
+                You can use the built-in JavaScript map method for arrays to iterate over each item of the list and
+                return a new version of each:
+            </p>
         </header>
         <div>
 
+            <Gist id='f2efb2665cebca927b141f9ff34e0433' />
+
             <h3>Examples (check the source code)</h3>
             <section>
-
+                <Component1 />
             </section>
             <hr/>
             <section>
-
+                <Component2 />
             </section>
             <hr/>
             <section>
@@ -26,5 +31,61 @@ function View_ListsInReact() {
         </div>
     </div>
 }
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
+function Component1() {
+    return (
+        <div>
+            {list.map(function(item) {
+                return (
+                    <div key={item.objectID}>
+                        {item.title}
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+//By assigning a key attribute to each list item’s element, React can
+//identify modified items if the list changes (e.g. re-ordering).
+
+function Component2() {
+    return (
+        <div>
+            {list.map(function(item) {
+                return (
+                    <div key={item.objectID}>
+                        <span>
+                        <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
 
 export default View_ListsInReact;
